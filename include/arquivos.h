74c67dc8c9bb2f9f06e515ba4cc7932aeebcff48
mSaullo <103381315+Euosaullo@users.noreply.github.com>
2026-07-04 06:01:31 -0300

    Fix comments and function prototypes in arquivos.h

--- include/arquivos.h
+++ include/arquivos.h
@@ -1,4 +1,4 @@
-Aqui deve ficar os protótipos das funções de arquivo:
+# Aqui deve ficar os protótipos das funções de arquivo:
 
-void salvarBiblioteca
-void carregarBiblioteca
+salvarBiblioteca
+carregarBiblioteca
