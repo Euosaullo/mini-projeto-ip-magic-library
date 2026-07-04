0ce0661ca12cebb91dc3c50aa0e744e7e0cd2a2b
mSaullo <103381315+Euosaullo@users.noreply.github.com>
2026-07-04 06:08:42 -0300

    Update structs.h

--- include/structs.h
+++ include/structs.h
@@ -1,1 +1,28 @@
-# Aqui deve ficar todas as structs do projeto 
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+#define TAM_BIBLIOTECA 100
+#define TAM_TEXTO 100
+
+typedef struct
+{
+    int dia;
+    int mes;
+    int ano;
+} Data;
+
+typedef struct
+{
+    char nome[TAM_TEXTO];
+    Data data_nascimento;
+} Autor;
+
+typedef struct
+{
+    int id;
+    char titulo[TAM_TEXTO];
+    Autor autor;
+    Data data_escrita;
+} LivroMagico;
+
+#endif
