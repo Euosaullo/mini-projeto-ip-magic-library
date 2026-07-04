1ea2c526e136a37136df26eb3fdfe927c3c82228
mSaullo <103381315+Euosaullo@users.noreply.github.com>
2026-07-04 06:11:57 -0300

    Remove unused function declarations from library.h

--- include/library.h
+++ include/library.h
@@ -4,15 +4,10 @@
 #include "structs.h"
 
 void inicializarVetor(LivroMagico **biblioteca);
-
 void cadastrarLivro(LivroMagico **biblioteca);
-
 void deletarLivro(LivroMagico **biblioteca, int id);
-
 void mostrarLivro(LivroMagico **biblioteca, int id);
-
 void editarLivro(LivroMagico **biblioteca, int id);
-
 void listarTitulos(LivroMagico **biblioteca);
 
 #endif
