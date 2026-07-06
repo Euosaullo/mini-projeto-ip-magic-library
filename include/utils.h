1865e4674bf27df6ab36c40ba96e0ade2e762845
mSaullo <103381315+Euosaullo@users.noreply.github.com>
2026-07-06 06:04:48 -0300

    Update utils.h

--- include/utils.h
+++ include/utils.h
@@ -1,1 +1,19 @@
+/* Declares shared utility functions used by the Magic Library system. */
+#ifndef UTILS_H
+#define UTILS_H
 
+#include <stdio.h>
+
+void clearInputBuffer(void);
+
+int removeLineBreak(char str[]);
+
+void discardRestOfLine(FILE *file);
+
+int readLine(const char *message, char str[], int size);
+
+void copyText(char destination[], const char source[], int size);
+
+int isValidDate(int day, int month, int year);
+
+#endif
