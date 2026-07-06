b5090dee94b91867db2de9c059afd1913df9ec44
mSaullo <103381315+Euosaullo@users.noreply.github.com>
2026-07-06 05:32:33 -0300

    Refine comment in encryption.c
    Updated comment to clarify encryption and decryption operations.

--- src/encryption.c
+++ src/encryption.c
@@ -1,2 +1,21 @@
-/* Implements string encryption and decryption using the 255 complement method. */
+/* Implements string encryption and decryption operations. */
+
 #include "encryption.h"
+
+void encryptString(char *str)
+{
+    int i;
+
+    i = 0;
+
+    while (str[i] != '\0')
+    {
+        str[i] = (char)(255 - (unsigned char) str[i]);
+        i++;
+    }
+}
+
+void decryptString(char *str)
+{
+    encryptString(str);
+}
