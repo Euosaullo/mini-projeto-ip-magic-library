8b3ead870baf7f11c4b9ca3954ae4925eca5e4d6
mSaullo <103381315+Euosaullo@users.noreply.github.com>
2026-07-06 06:29:07 -0300

    Add functions for writing and reading book attributes

--- src/files.c
+++ src/files.c
@@ -14,7 +14,11 @@ static void writeEncryptedTitle(FILE *file, const char title[]);
 static int readEncryptedTitle(FILE *file, char title[]);
 static int decodeEncryptedTitle(const char source[], char destination[]);
 static int hexDigitValue(char character);
+static void writeBookAttributes(FILE *file, const BookAttributes *attributes);
+static int readBookAttributesFromFile(FILE *file, BookAttributes *attributes);
 static int isLoadedBookValid(MagicBook **library, const MagicBook *book);
+static int isBookAttributesValid(const BookAttributes *attributes);
+static int isOptionalAttributeValid(int hasAttribute, int value);
 
 void saveLibraryToFile(MagicBook **library, const char *fileName)
 {
@@ -49,6 +53,8 @@ void saveLibraryToFile(MagicBook **library, const char *fileName)
                     book->writingDate.day,
                     book->writingDate.month,
                     book->writingDate.year);
+
+            writeBookAttributes(file, &book->attributes);
         }
     }
 
@@ -140,6 +146,12 @@ void loadLibraryFromFile(MagicBook **library, const char *fileName)
         }
 
         discardRestOfLine(file);
+
+        if (!readBookAttributesFromFile(file, &newBook->attributes))
+        {
+            free(newBook);
+            break;
+        }
 
         if (!isLoadedBookValid(library, newBook))
         {
@@ -244,6 +256,55 @@ static int hexDigitValue(char character)
     return -1;
 }
 
+static void writeBookAttributes(FILE *file, const BookAttributes *attributes)
+{
+    fprintf(file, "%d %d %d %d %d %d %d %d %d %d %d %d %d %d\n",
+            attributes->hasStrength,
+            attributes->strength,
+            attributes->hasDexterity,
+            attributes->dexterity,
+            attributes->hasConstitution,
+            attributes->constitution,
+            attributes->hasIntelligence,
+            attributes->intelligence,
+            attributes->hasWisdom,
+            attributes->wisdom,
+            attributes->hasCharisma,
+            attributes->charisma,
+            attributes->hasMagic,
+            attributes->magic);
+}
+
+static int readBookAttributesFromFile(FILE *file, BookAttributes *attributes)
+{
+    int result;
+
+    result = fscanf(file, "%d %d %d %d %d %d %d %d %d %d %d %d %d %d",
+                    &attributes->hasStrength,
+                    &attributes->strength,
+                    &attributes->hasDexterity,
+                    &attributes->dexterity,
+                    &attributes->hasConstitution,
+                    &attributes->constitution,
+                    &attributes->hasIntelligence,
+                    &attributes->intelligence,
+                    &attributes->hasWisdom,
+                    &attributes->wisdom,
+                    &attributes->hasCharisma,
+                    &attributes->charisma,
+                    &attributes->hasMagic,
+                    &attributes->magic);
+
+    if (result != 14)
+    {
+        return 0;
+    }
+
+    discardRestOfLine(file);
+
+    return 1;
+}
+
 static int isLoadedBookValid(MagicBook **library, const MagicBook *book)
 {
     if (book->id <= 0 || bookIdExists(library, book->id))
@@ -270,5 +331,65 @@ static int isLoadedBookValid(MagicBook **library, const MagicBook *book)
         return 0;
     }
 
+    if (!isBookAttributesValid(&book->attributes))
+    {
+        return 0;
+    }
+
     return 1;
+}
+
+static int isBookAttributesValid(const BookAttributes *attributes)
+{
+    if (!isOptionalAttributeValid(attributes->hasStrength, attributes->strength))
+    {
+        return 0;
+    }
+
+    if (!isOptionalAttributeValid(attributes->hasDexterity, attributes->dexterity))
+    {
+        return 0;
+    }
+
+    if (!isOptionalAttributeValid(attributes->hasConstitution, attributes->constitution))
+    {
+        return 0;
+    }
+
+    if (!isOptionalAttributeValid(attributes->hasIntelligence, attributes->intelligence))
+    {
+        return 0;
+    }
+
+    if (!isOptionalAttributeValid(attributes->hasWisdom, attributes->wisdom))
+    {
+        return 0;
+    }
+
+    if (!isOptionalAttributeValid(attributes->hasCharisma, attributes->charisma))
+    {
+        return 0;
+    }
+
+    if (!isOptionalAttributeValid(attributes->hasMagic, attributes->magic))
+    {
+        return 0;
+    }
+
+    return 1;
+}
+
+static int isOptionalAttributeValid(int hasAttribute, int value)
+{
+    if (hasAttribute != 0 && hasAttribute != 1)
+    {
+        return 0;
+    }
+
+    if (!hasAttribute)
+    {
+        return value == 0;
+    }
+
+    return value >= ATTRIBUTE_MIN && value <= ATTRIBUTE_MAX;
 }
