eb06efc27cc2de7a797e30e4f9cf65459a7a8d01
mSaullo <103381315+Euosaullo@users.noreply.github.com>
2026-07-06 06:03:21 -0300

    Reorder and update function declarations in library.h

--- include/library.h
+++ include/library.h
@@ -4,16 +4,24 @@
 
 #include "structs.h"
 
-void initializeLibrary (MagicBook **library);
+void initializeLibrary(MagicBook **library);
 
-void registerBook (MagicBook **library);
+void freeLibrary(MagicBook **library);
 
-void deleteBookById (MagicBook **library, int id);
+void registerBook(MagicBook **library);
 
-void displayBookById (MagicBook **library, int id);
+void deleteBookById(MagicBook **library, int id);
 
-void updateBookById (MagicBook **library, int id);
+void displayBookById(MagicBook **library, int id);
 
-void listBookTitles (MagicBook **library);
+void updateBookById(MagicBook **library, int id);
+
+void listBookTitles(MagicBook **library);
+
+int findFreeLibrarySlot(MagicBook **library);
+
+int findBookIndexById(MagicBook **library, int id);
+
+int bookIdExists(MagicBook **library, int id);
 
 #endif
